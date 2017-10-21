789876de53b715567c63b60cb1cdfb949bf37e99
Umesh Singla <umeshksingla@gmail.com>
2017-10-22 02:12:31 +0530

    Add FIFO scheduling algo
    It doesn't try to simulate router in real time but just outputs
    the order of packets received at destination, simply it's the
    packets sorted by their arrival time

---
+++ fifo.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python3
+
+'''
+File:		 fifo.py
+Description: FIFO scheduling algorithm for a router
+Author:		 Umesh Singla
+Date:		 Oct 21, 2017
+Python:		 v3.6.3
+'''
+
+from utils import initialize
+
+
+def run():
+	"""
+	"""
+	sources, packets = initialize()
+	order = sorted(packets, key=lambda x: x.arrivalTime)
+	for p in order:
+		print(p.source, end=' ')
+	print()
+
+
+if __name__ == '__main__':
+	run()
